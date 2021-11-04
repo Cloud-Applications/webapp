b8d70ea9ad4ef2538bf7dbd348e5795378f49c8a
Harshika Gupta <gupta.harshik@northeastern.edu>
2021-11-04 10:40:03 -0400

    edited package

--- codeDeploy/applicationStart.sh
+++ codeDeploy/applicationStart.sh
@@ -2,5 +2,6 @@
 
 cd /home/ubuntu/webapp
 pwd
+ls
 # sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/amazon-cloudwatch-agent.json -s
 pm2 start api.js
