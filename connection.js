17f215d072de9e66605a98f96a54f5989d2e55f9
Harshika Gupta <gupta.harshik@northeastern.edu>
2021-12-01 04:21:44 -0500

    work

--- connection.js
+++ connection.js
@@ -1,7 +1,5 @@
-const logger = require('./logger');
 require('dotenv').config();
 const {Client} = require('pg')
-logger.info({env: process.env, msg: 'test in connection'});
 const db = new Client({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USERNAME || 'postgres',
@@ -9,8 +7,6 @@ const db = new Client({
     password: process.env.DB_PASSWORD || 'Harshika@123',
     database: process.env.DB_NAME || 'postgres',
     metrics_hostname: "localhost",
-    topic: process.env.TOPIC_ARN,
-    domain: process.env.DOMAIN_NAME,
     metrics_port: 8125
 })
 
--- controllers/verifyUsers.js
+++ controllers/verifyUsers.js
@@ -3,6 +3,26 @@ const logger = require('../logger');
 
 const verifyUsers = (req, res) => {
     console.log(req, res);
+    logger.info({req: req, msg: 'request body', res: res, msg: 'msg response', request: req.request});
+    const a = req.request._parsedUrl.query;
+    const username = a.split("=")[1].split("&")[0]
+    const text = 'UPDATE public.users SET verified = $1, verified_on = $2 WHERE username =$3'
+    const values = [true, new Date().toISOString(), username];
+    client.query(text1, value1, (error, results) => {
+        if(error) {
+            logger.error('Error while verifying user');
+            return res.status(400).json({
+                status: 400,
+                error: err
+            });
+        } else {
+            logger.info('User verified successfully');
+            return res.status(204).json({
+                status: 204,
+                description: 'User verified successfully'
+            });
+        }
+    });
 }
 
 exports.verifyUsers = verifyUsers;
